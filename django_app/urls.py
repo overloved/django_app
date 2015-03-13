dc43feedf181fcc86921c0d15b6b712ed8f7f03e
yao <yhuang76@jhu.edu>
2015-03-12 23:20:22 -0400

    datetime update

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django_app.views import hello, current_datetime
+from django_app.views import hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
